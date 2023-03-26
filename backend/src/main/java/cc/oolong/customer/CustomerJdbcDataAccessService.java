c54c9598d104f014d59a43d06fac4d91f87d121c
mingfai <mingfai@live.com>
2023-03-26 23:15:24 +0800

    break existsPersonWithEmail

--- backend/src/main/java/cc/oolong/customer/CustomerJdbcDataAccessService.java
+++ backend/src/main/java/cc/oolong/customer/CustomerJdbcDataAccessService.java
@@ -57,7 +57,7 @@ public class CustomerJdbcDataAccessService implements  CustomerDao {
         var sql= """
                 SELECT count(id)
                 FROM customer
-                WHERE email=?
+                WHERE name=?
                 """;
 
         Integer count=jdbcTemplate.queryForObject(sql,Integer.class,email);
