1822cd868c4ca96d4291b3eca89e66096279c9cb
mingfai <mingfai@live.com>
2023-03-26 21:35:34 +0800

    added a new line and removed unused import

--- backend/src/main/java/cc/oolong/Main.java
+++ backend/src/main/java/cc/oolong/Main.java
@@ -8,13 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Random;
 
 @SpringBootApplication
@@ -22,6 +15,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         ConfigurableApplicationContext ctx = SpringApplication.run(Main.class, args);
 //        printBeans(ctx);
     }
